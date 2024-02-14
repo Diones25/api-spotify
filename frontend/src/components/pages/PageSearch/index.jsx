692bbed88f87a97fa431cf9ec8c2c92267a9f31b
Diones <pereiradiones987@gmail.com>
2024-02-14 14:23:15 -0300

    adicionando margin

--- frontend/src/components/pages/PageSearch/index.jsx
+++ frontend/src/components/pages/PageSearch/index.jsx
@@ -19,38 +19,40 @@ const PageSearch = () => {
             Navegar por todas as seções
           </Typography>
         </Box>
-        <Grid container spacing={2} columns={14}>
-          <Grid item xs={12} sm={6} md={3} lg={2}>
-            <CardCategory 
-              image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
-              title={'Sertanejo'}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={2}>
-            <CardCategory 
-              image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
-              title={'Sertanejo'}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={2}>
-            <CardCategory 
-              image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
-              title={'Sertanejo'}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={2}>
-            <CardCategory 
-              image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
-              title={'Sertanejo'}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} md={3} lg={2}>
-            <CardCategory 
-              image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
-              title={'Sertanejo'}
-            />
-          </Grid>
-        </Grid> 
+        <div style={{ marginTop: '25px' }}>
+          <Grid container spacing={2} columns={14}>
+            <Grid item xs={12} sm={6} md={3} lg={2}>
+              <CardCategory 
+                image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
+                title={'Sertanejo'}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} lg={2}>
+              <CardCategory 
+                image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
+                title={'Sertanejo'}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} lg={2}>
+              <CardCategory 
+                image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
+                title={'Sertanejo'}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} lg={2}>
+              <CardCategory 
+                image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
+                title={'Sertanejo'}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3} lg={2}>
+              <CardCategory 
+                image={"https://t.scdn.co/images/8e6eaf8b5c9449fdbb689a4fdace1f25.jpeg"}
+                title={'Sertanejo'}
+              />
+            </Grid>
+          </Grid> 
+        </div>
       </Sidebar>
     </>
   )
