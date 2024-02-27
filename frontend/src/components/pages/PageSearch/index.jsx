a62eb9d0d6193418f63f5896022de4e84496710f
Diones <pereiradiones987@gmail.com>
2024-02-27 09:11:39 -0300

    update

--- frontend/src/components/pages/PageSearch/index.jsx
+++ frontend/src/components/pages/PageSearch/index.jsx
@@ -88,17 +88,61 @@ const PageSearch = () => {
           container spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          <Grid item lg={2}>
-            <div className="Container_buttons">
-              <button className="active" onClick={() => handleTypeChange('artist,album,playlist,track')}>Tudo</button>
-              <button onClick={() => handleTypeChange('artist')}>Artistas</button>
-              <button onClick={() => handleTypeChange('playlist')}>Playlists</button>
-              <button onClick={() => handleTypeChange('track')}>Músicas</button>
-              <button onClick={() => handleTypeChange('album')}>Álbuns</button>
-            </div>
-          </Grid>
+
+          {query &&
+            <Grid item lg={2}>
+              <div className="Container_buttons">
+                <button className="active" onClick={() => handleTypeChange('artist,album,playlist,track')}>Tudo</button>
+                <button onClick={() => handleTypeChange('artist')}>Artistas</button>
+                <button onClick={() => handleTypeChange('playlist')}>Playlists</button>
+                <button onClick={() => handleTypeChange('track')}>Músicas</button>
+                <button onClick={() => handleTypeChange('album')}>Álbuns</button>
+              </div>
+            </Grid>          
+          }
         </Grid>
-      </div>
+      </div>      
+
+      <div className="ms-sm-0 ms-md-0 ms-lg-4 mt-5">
+        <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
+          Musicas
+        </div>
+        
+        <div className="container-fluid d-flex flex-column">
+          <div className="row">
+            <div className="col-md-12 col-lg-12">
+         
+              <div className="col-md-6">
+                <iframe 
+                  style={{ borderRadius: "12px" }} 
+                  src="https://open.spotify.com/embed/track/7iL6o9tox1zgHpKUfh9vuC?utm_source=generator&theme=0" 
+                  width="100%" 
+                  height="152" 
+                  frameBorder="0" 
+                  allowfullscreen="" 
+                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture;" 
+                  loading="lazy">
+                </iframe>
+              </div>
+              
+              <div className="col-md-6">
+                <iframe 
+                  style={{ borderRadius: "12px" }} 
+                  src="https://open.spotify.com/embed/track/7iL6o9tox1zgHpKUfh9vuC?utm_source=generator&theme=0" 
+                  width="100%" 
+                  height="152" 
+                  frameBorder="0" 
+                  allowfullscreen="" 
+                  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                  loading="lazy">
+                </iframe>
+              </div>
+          
+            </div>
+          </div>                    
+        </div>
+
+      </div> 
         
       <div className="ms-sm-0 ms-md-0 ms-lg-4 mt-5">
         <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
@@ -148,6 +192,51 @@ const PageSearch = () => {
       <div className="ms-sm-0 ms-md-0 ms-lg-4 mt-5">
         <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
           Albuns
+        </div>
+
+        <Grid 
+          className="
+            d-flex justify-content-center 
+            justify-content-sm-center 
+            justify-content-md-center 
+            justify-content-lg-start
+            flex-column
+            flex-sm-row
+            flex-md-row
+            flex-lg-row
+            align-items-center
+          " 
+            container spacing={{ xs: 2, md: 3 }} 
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+          <Grid item lg={2}>
+            <Card
+              image={"https://i.scdn.co/image/ab67616d00001e02d843fabb75fef14010e30cae"}
+              title={'Get Rich Or Die Tryin'}
+              description={'2003 . 50 Cent'}
+            />
+          </Grid>
+          <Grid item lg={2}>
+            <Card
+              image={"https://i.scdn.co/image/ab67616d00001e0291f7222996c531b981e7bb3d"}
+              title={'The Massacre'}
+              description={'2005 . 50 Cent'}
+            />
+          </Grid>
+
+          <Grid item lg={2}>
+            <Card
+              image={"https://i.scdn.co/image/ab67616d00001e02f4f2ccbae0c1905fcaf75b77"}
+              title={'Barões da Pis...'}
+              description={'2017 . 50 Cent'}
+            />
+          </Grid>
+        </Grid>
+      </div>
+
+      <div className="ms-sm-0 ms-md-0 ms-lg-4 mt-5">
+        <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
+        Playlists
         </div>
 
         <Grid 
