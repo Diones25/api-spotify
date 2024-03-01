cacd0dc5eaf7566747944602f7b073a51547beec
Diones <pereiradiones987@gmail.com>
2024-03-01 11:35:38 -0300

    voltando a mostrar a página home

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -3,7 +3,8 @@ const spotifyController = require('../controllers/spotifyController.js');
 
 const router = Router();
 
-router.get('/search', spotifyController.search);;
+router.get('/', spotifyController.home);
+router.get('/search', spotifyController.search);
 router.get('/lyrics', spotifyController.getLyrics);
 
 module.exports = router;
