202c8edc85a4ad2cd6c16ddd79198dffcca3a37c
Diones <pereiradiones987@gmail.com>
2024-03-01 00:19:29 -0300

    rendenrizando na tela os albums e playlists

--- frontend/src/components/pages/PageSearch/index.jsx
+++ frontend/src/components/pages/PageSearch/index.jsx
@@ -104,45 +104,6 @@ const PageSearch = () => {
         </Grid>
       </div>     
 
-      <div>
-        {/* Render search results here */}
-        {/* {results && searchType === 'artist'&& results.length > 0 &&(
-          <div>
-            <h2>Artists</h2>
-            {results.map((artist) => (
-              <div key={artist.id}>
-                <p>{artist.name}</p>
-                <p>{artist.image}</p>
-                <img src={artist.image} alt="" srcset="" />
-              </div>
-            ))}
-          </div>
-        )}   */}
-
-        {results && results.playlists && (
-          <div>
-            <h2>Playlists</h2>
-            {results.playlists.items.map((playlist) => (
-              <div key={playlist.id}>
-                <p>{playlist.name}</p>
-              </div>
-            ))}
-          </div>
-        )}
-
-        {results && results.albums && (
-          <div>
-            <h2>Albums</h2>
-            {results.albums.items.map((album) => (
-              <div key={album.id}>
-                <p>{album.name}</p>
-              </div>
-            ))}
-          </div>
-        )}
-
-
-      </div> 
       {/*Colocar um loading enquando as mucias carregam*/}
       {results && results.tracks && (
         <div className="ms-sm-0 ms-md-0 ms-lg-4 mt-5">
@@ -168,12 +129,10 @@ const PageSearch = () => {
                       loading="lazy">
                     </iframe>
                   </div>
-                ))}
-            
+                ))}            
               </div>
             </div>                    
           </div>
-
         </div>           
       )}
         
@@ -213,93 +172,74 @@ const PageSearch = () => {
       </div> 
 
       <div className="ms-sm-0 ms-md-0 ms-lg-4 mt-5">
-        <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
-          Albuns
-        </div>
-
-        <Grid 
-          className="
-            d-flex justify-content-center 
-            justify-content-sm-center 
-            justify-content-md-center 
-            justify-content-lg-start
-            flex-column
-            flex-sm-row
-            flex-md-row
-            flex-lg-row
-            align-items-center
-          " 
-            container spacing={{ xs: 2, md: 3 }} 
-            columns={{ xs: 4, sm: 8, md: 12 }}
-          >
-          <Grid item lg={2}>
-            <Card
-              image={"https://i.scdn.co/image/ab67616d00001e02d843fabb75fef14010e30cae"}
-              title={'Get Rich Or Die Tryin'}
-              description={'2003 . 50 Cent'}
-            />
-          </Grid>
-          <Grid item lg={2}>
-            <Card
-              image={"https://i.scdn.co/image/ab67616d00001e0291f7222996c531b981e7bb3d"}
-              title={'The Massacre'}
-              description={'2005 . 50 Cent'}
-            />
-          </Grid>
-
-          <Grid item lg={2}>
-            <Card
-              image={"https://i.scdn.co/image/ab67616d00001e02f4f2ccbae0c1905fcaf75b77"}
-              title={'Barões da Pis...'}
-              description={'2017 . 50 Cent'}
-            />
-          </Grid>
-        </Grid>
+        {results && searchType === 'album'&& results.length > 0 &&(
+          <>
+            <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
+              Albuns
+            </div>
+            <Grid 
+              className="
+                d-flex justify-content-center 
+                justify-content-sm-center 
+                justify-content-md-center 
+                justify-content-lg-start
+                flex-column
+                flex-sm-row
+                flex-md-row
+                flex-lg-row
+                align-items-center
+              " 
+                container spacing={{ xs: 2, md: 3 }} 
+                columns={{ xs: 4, sm: 8, md: 12 }}
+                >
+                {results.map((album) => (
+                  <Grid item lg={2}>
+                    <Card
+                      image={album.image ? album.image : image_notFound}
+                      title={album.name}
+                      description={album.type ? 'Album' : ''}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
+          
+          </>
+        )}
       </div>
 
       <div className="ms-sm-0 ms-md-0 ms-lg-4 mt-5">
-        <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
-        Playlists
-        </div>
-
-        <Grid 
-          className="
-            d-flex justify-content-center 
-            justify-content-sm-center 
-            justify-content-md-center 
-            justify-content-lg-start
-            flex-column
-            flex-sm-row
-            flex-md-row
-            flex-lg-row
-            align-items-center
-          " 
-            container spacing={{ xs: 2, md: 3 }} 
-            columns={{ xs: 4, sm: 8, md: 12 }}
-          >
-          <Grid item lg={2}>
-            <Card
-              image={"https://i.scdn.co/image/ab67616d00001e02d843fabb75fef14010e30cae"}
-              title={'Get Rich Or Die Tryin'}
-              description={'2003 . 50 Cent'}
-            />
-          </Grid>
-          <Grid item lg={2}>
-            <Card
-              image={"https://i.scdn.co/image/ab67616d00001e0291f7222996c531b981e7bb3d"}
-              title={'The Massacre'}
-              description={'2005 . 50 Cent'}
-            />
-          </Grid>
-
-          <Grid item lg={2}>
-            <Card
-              image={"https://i.scdn.co/image/ab67616d00001e02f4f2ccbae0c1905fcaf75b77"}
-              title={'Barões da Pis...'}
-              description={'2017 . 50 Cent'}
-            />
-          </Grid>
-        </Grid>
+        {results && searchType === 'playlist'&& results.length > 0 &&(
+          <>
+            <div id="Title" className="fw-bold h3 text-center text-sm-center text-md-center text-lg-start">
+              Playlists
+            </div>
+            <Grid 
+              className="
+                d-flex justify-content-center 
+                justify-content-sm-center 
+                justify-content-md-center 
+                justify-content-lg-start
+                flex-column
+                flex-sm-row
+                flex-md-row
+                flex-lg-row
+                align-items-center
+              " 
+                container spacing={{ xs: 2, md: 3 }} 
+                columns={{ xs: 4, sm: 8, md: 12 }}
+              >
+              {results.map((playlist) => (
+                <Grid item lg={2}>
+                  <Card
+                    image={playlist.image ? playlist.image : image_notFound}
+                    title={playlist.name}
+                    description={playlist.type ? 'Playlist' : ''}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </>
+        )}
       </div>
     </>
   )
