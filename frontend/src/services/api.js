0a3699b56a65d47f96a47210338e10633d0f1d90
Diones <pereiradiones987@gmail.com>
2024-03-06 23:00:50 -0300

    atualizando endpoint base

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 
-const baseUrl = 'http://localhost:4001';
+const baseUrl = 'http://18.228.38.54:3333';
 
 export async function search(q, type){
   const response = await fetch(`${baseUrl}/search/?type=${type}&q=${q}`);
