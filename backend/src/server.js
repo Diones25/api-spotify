0ffd8091276ca5280016dca6e5ecda9135b6313b
Diones <pereiradiones987@gmail.com>
2024-03-08 15:21:43 -0300

    testando configuração de cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(port, () => {
   console.log(chalk.bgGreenBright.black(`Servidor rodando no endereço: http://localhost:${port}`));
 });
